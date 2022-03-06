e6c9b195cf66c6cdcebdaf1138d7c4cee0b68f9f
Unit335 <61664740+Unit335@users.noreply.github.com>
2022-03-06 23:39:06 +0300

    Minor fix

--- test_task/views.py
+++ test_task/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 def rekruto(request):
     name = request.GET.get("name", "")
     msg = request.GET.get("message", "")
+    err = ''
     if not msg:
-        msg = 'You should forward message via \'message\' parameter.'
+        err = 'You should forward message via \'message\' parameter.'
     if not name:
         name = 'Anonym'
-        msg = msg + '\nName should be forwarded via \'name\' parameter'
-    return render(request, 'index.html', {'title': name, 'cal': msg})
+        err = err + '\nName should be forwarded via \'name\' parameter'
+    return render(request, 'index.html', {'title': name, 'msg': msg, 'error': err})
